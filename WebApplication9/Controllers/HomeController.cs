de955c5a8fdea670eafc86f3433f43258ce6a6a2
shamli1102 <110428881+shamli1102@users.noreply.github.com>
2022-08-02 16:44:07 +0530

    Infilon tech task

---
+++ WebApplication9/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebApplicationEntities1 _db;
+
+        public HomeController()
+        {
+            _db = new WebApplicationEntities1();
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var json = "";
+
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString("https://jsonplaceholder.typicode.com/todos/");
+                //json = json.Replace("[","").Replace("]","");
+                List<dynamic> models = JsonConvert.DeserializeObject<List<dynamic>>(json);
+                foreach (dynamic im in models)
+                {
+
+                }
+            }
+                    ViewData.Model = _db.Employee.ToList();
+            return View();
+        }
+    }
+}
